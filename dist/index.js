ff939f71902db893f9dbd8dc7d80083bcc04ff23
titito2005 <luis.4.2000@hotmail.com>
2024-08-20 18:22:06 -0600

    Initial commit

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Button = void 0;
-// src/index.ts
-var Button_1 = require("./components/Button");
-Object.defineProperty(exports, "Button", { enumerable: true, get: function () { return Button_1.Button; } });
