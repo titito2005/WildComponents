909d0e01f38837422d2edafb5785bcf3efd82103
titito2005 <luis.4.2000@hotmail.com>
2024-08-28 18:51:26 -0600

    feat: [IS-23] Create base button component

--- src/components/atoms/button/Button.styled.tsx
+++ src/components/atoms/button/Button.styled.tsx
@@ -1,9 +1,39 @@
 import styled from 'styled-components';
+import { BaseButtonProps } from './Button';
 
-export const PrettyButton = styled.button`
-  background-color: pink;
-  border: 2px solid pink;
-  border-radius: 5px;
-  color: black;
-  padding: 10px;
+export const Basebutton = styled.button<BaseButtonProps>`
+  ${({
+    rounded,
+    filled,
+    padding = '15px',
+    margin,
+    backgroungColor = '600',
+    hoverColor = '800',
+    color = 'white',
+    theme,
+  }) => `
+    background: ${
+      filled
+        ? `linear-gradient(45deg, ${theme.colors[backgroungColor]}, ${theme.colors[hoverColor]})`
+        : 'transparent'
+    };
+    border: 2px solid ${theme.colors[backgroungColor]};
+    border-radius: ${rounded ? '10px' : '0px'};
+    padding: ${padding || '10px'};
+    color: ${filled ? color : theme.colors[backgroungColor]};
+    cursor: pointer;
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2);
+    margin: ${margin};
+
+    &:hover {
+      background: ${
+        filled
+          ? `linear-gradient(45deg, ${theme.colors[hoverColor]}, ${theme.colors[backgroungColor]})`
+          : 'transparent'
+      };
+      color: ${filled ? 'white' : theme.colors[hoverColor]};
+      border: 2px solid ${theme.colors[hoverColor]};
+      box-shadow: 0px 6px 12px rgba(0, 0, 0, 0.3);
+    }
+  `}
 `;
