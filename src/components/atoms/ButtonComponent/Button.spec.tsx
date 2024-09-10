c335a09fdcce370642492ff7e86af48359573a6f
titito2005 <luis.4.2000@hotmail.com>
2024-09-09 22:35:18 -0600

    fix: [IS-40] Create Icon Button (Update button)

--- src/components/atoms/ButtonComponent/Button.spec.tsx
+++ src/components/atoms/ButtonComponent/Button.spec.tsx
@@ -4,14 +4,14 @@ import { Button } from './Button';
 
 describe('Button Component', () => {
   it('renders Button component with correct label', () => {
-    render(<Button label="Click me" onClick={() => {}} />);
+    render(<Button children="Click me" onClick={() => {}} />);
     const buttonElement = screen.getByText(/Click me/i);
     expect(buttonElement).toBeInTheDocument();
   });
 
   it('button click triggers the onClick event', () => {
     const handleClick = jest.fn();
-    render(<Button label="Click me" onClick={handleClick} />);
+    render(<Button children="Click me" onClick={handleClick} />);
     const buttonElement = screen.getByText(/Click me/i);
     buttonElement.click();
     expect(handleClick).toHaveBeenCalledTimes(1);
